600d5874aa79c3df00f21847243fc0872c16794d
Vincent Duarte <vincentdu@theodo.fr>
2024-05-29 09:26:17 +0200

    come back

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,27 @@
 "use client"
+import axios from 'axios';
 import { useEffect, useState } from "react";
 
-
-// export const getServerSideProps = async () => {
-//   const res = await fetch('http://3000/api/hello'); // Remplacez cette URL par celle de votre API
-//   const data = await res.json();
-//   console.log("respons = " + res);
-
-//   return {
-//     props: {
-//       name: data.pseudo, // Assurez-vous que la réponse de votre API contient un champ `firstName`
-//     },
-//   };
-// };
-
 export default function Home() {
-  const [pseudo, setFirstName] = useState<string>('');
+
+  /*
+  http://localhost:3000
+
+  https://test-cors-drab.vercel.app/api/hello
+
+  https://test-cors-second.vercel.app/api/hello
+  */
+
+  const routeCallAuto = 'https://test-cors-drab.vercel.app/api/hello';
+  const routeGetAlice = 'https://test-cors-drab.vercel.app/api/hello';
+  const routeDeleteBob = 'https://test-cors-drab.vercel.app/api/hello';
+
+
+  const [pseudoGetAlice, setFirstName] = useState<string>('');
 
   useEffect(() => {
     const fetchFirstName = async () => {
-      const res = await fetch('https://test-cors-drab.vercel.app/api/hello');
+      const res = await fetch(routeCallAuto);
       console.log("respons = " + res);
       const data = await res.json();
       setFirstName(data.pseudo);
@@ -27,20 +29,80 @@ export default function Home() {
 
     fetchFirstName();
   }, []);
+
+  const [dataAlice, setDataAlice] = useState<string | null>(null);
+  const [loadingAlice, setLoadingAlice] = useState<boolean>(false);
+  const [errorAlice, setErrorAlice] = useState<string | null>(null);
+  const handleClickAlice = async () => {
+    setLoadingAlice(true);
+    setErrorAlice(null);
+    try {
+      const response = await axios.get(routeGetAlice); // Remplacez '/api/data' par votre URL de l'API
+      setDataAlice(response.data.pseudo);
+    } catch (error) {
+      setErrorAlice('Erreur lors de la récupération des données');
+    } finally {
+      setLoadingAlice(false);
+    }
+  };
+
+  const [dataBob, setDataBob] = useState<string | null>(null);
+  const [loadingBob, setLoadingBob] = useState<boolean>(false);
+  const [errorBob, setErrorBob] = useState<string | null>(null);
+  const handleClickBob = async () => {
+    setLoadingBob(true);
+    setErrorBob(null);
+    try {
+      const response = await axios.delete(routeDeleteBob); // Remplacez '/api/data' par votre URL de l'API
+      setDataBob(response.data.pseudo);
+    } catch (error) {
+      setErrorBob('Erreur lors de la récupération des données');
+    } finally {
+      setLoadingBob(false);
+    }
+  };
   
   return (
     <main>
 
       <h1 style={{ color: 'green', textAlign: 'center' }}>Mon site</h1>
 
+      <h3 style={{ color: 'brown', textAlign: 'center' }}>Boutons :</h3>
       <div style={{textAlign: 'center'}}>
-          <p> Je suis : {pseudo}</p>
+          <p> Call auto : {pseudoGetAlice}</p>
       </div>
 
       <div style={{textAlign: 'center'}}>
-        <h4>Nouveau message :</h4>
-        <p> Message de Charlie : <a href="http://localhost:6660/">Va voir cet autre site sur les CORS !! Aller clique il est top !!!</a></p>
+      <h3>Alice :</h3>
+      <button onClick={handleClickAlice}>GET</button>
+      {loadingAlice && <p>Chargement...</p>}
+      {errorAlice && <p>{errorAlice}</p>}
+      {dataAlice && <p>Données: {dataAlice}</p>}
       </div>
+
+      <div style={{textAlign: 'center'}}>
+      <h3>Bob :</h3>
+      <button onClick={handleClickBob}>DELETE</button>
+      {loadingBob&& <p>Chargement...</p>}
+      {errorBob && <p>{errorBob}</p>}
+      {dataBob && <p>Données: {dataBob}</p>}
+      </div>
+
+      <h3 style={{ color: 'purple', textAlign: 'center' }}>site online 1 :</h3>
+      <div style={{textAlign: 'center'}}>
+      <a href='https://test-cors-drab.vercel.app/' target="_blank">https://test-cors-drab.vercel.app/</a>
+      </div>
+
+      <h3 style={{ color: 'purple', textAlign: 'center' }}>site online 2 :</h3>
+      <div style={{textAlign: 'center'}}>
+      <a href='https://test-cors-second.vercel.app/' target="_blank">https://test-cors-second.vercel.app/</a>
+      </div>
+
+      <h3 style={{ color: 'purple', textAlign: 'center' }}>Online dashboard :</h3>
+      <div style={{textAlign: 'center'}}>
+      <a href='https://vercel.com/vincent-duartes-projects' target="_blank">https://vercel.com/vincent-duartes-projects</a>
+      </div>
+
     </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+//const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "https://test-cors-drab.vercel.app:72024" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
+};
+
+  /*
+  http://localhost:3000
+
+  https://test-cors-drab.vercel.app
+
+  https://test-cors-second.vercel.app
+  */
+
 
 export default nextConfig;
