7ba0e1d8b347ca00da46242828e468fc95680add
Vincent Duarte <vincentdu@theodo.fr>
2024-06-20 09:41:05 +0200

    feat: some progress 1

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -4,8 +4,6 @@ export async function GET (request: NextRequest){
     
     const alice = {
         'pseudo': "Alice",
-        'password': "MiamLesPommes",
-        'phone': "0611332022"
     }
 
     return NextResponse.json(alice);
--- next.config.mjs
+++ next.config.mjs
@@ -4,37 +4,17 @@ const nextConfig = {
     async headers() {
         return [
             {
-                // matching all API routes
                 source: "/api/:path",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Origin", value: "http://my-site.com" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,PATCH,POST,PUT,OPTIONS" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
             }
-            // ,
-            // {
-            //     // matching all API routes
-            //     source: "/api/cookie",
-            //     headers: [
-            //         { key: "Access-Control-Allow-Credentials", value: "true" },
-            //         { key: "Access-Control-Allow-Origin", value: "https://test-cors-second.vercel.app" }, // replace this your actual origin
-            //         { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-            //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            //     ]
-            // }
         ]
     }
 };
-
-  /*
-  http://localhost:3000
-
-  https://test-cors-drab.vercel.app
-
-  https://test-cors-second.vercel.app
-  */
 
 
 export default nextConfig;
